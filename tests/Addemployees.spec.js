5c801b10b12a1e865f4e20ecbd3cf12a124a9a61
Iamguruteja <gurutejaguruteja14@gmail.com>
2025-10-21 18:49:20 +0530

    commit

--- tests/Addemployees.spec.js
+++ tests/Addemployees.spec.js
@@ -2,4 +2,19 @@ import {test, expect} from '@playwright/test'
 
 test("verify the add employees functionalities", async ({ page }) =>{
 
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.locator('input[name="username"]').fill('Admin')
+    await page.locator("input[type='password']").fill("admin123")
+    await page.locator("//button[@type='submit']").click()
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+    await page.locator('//a[@href="/web/index.php/pim/viewPimModule"]').click()
+    await page.locator('//a[text()="Add Employee"]').click()
+    await page.locator('input[name="firstName"]').fill('guruteja')
+    await page.locator('input[name="lastName"]').fill("dodda thimmappa")
+    let r = (Math.random() + 1).toString(36).substring(7)
+    await page.locator('//input[@class="oxd-input oxd-input--active"]').last().fill(r)
+    await page.locator('button[type="submit"]').click()
+    await expect(page.locator('//a[text()="Personal Details"]')).toBeVisible()
+
 })
+
